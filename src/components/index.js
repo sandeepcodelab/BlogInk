ce8613c4608d15a591e928a48f30e0f9f8811d12
sandeepcodelab <sandeep.codes1@gmail.com>
2025-07-03 18:11:25 +0530

    add some new contents in this file

--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,7 @@
 import Header from "./Header/Header";
 import Footer from "./Footer/Footer";
+import LogoutBtn from "./Header/LogoutBtn";
+import Logo from "./Logo";
+import Container from "./Container/Container";
 
-export { Header, Footer };
+export { Header, Footer, Container, Logo, LogoutBtn };
