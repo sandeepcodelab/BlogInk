688ac0ebdfc2105dcc29be4bf8ac8f4e9e74b503
sandeepcodelab <sandeep.codes1@gmail.com>
2025-07-05 16:10:45 +0530

    add new PostCard component

---
+++ src/components/PostCard.jsx
@@ -0,0 +1,18 @@
+import appwriteService from '../appwrite/config'
+import { Link } from 'react-router'
+
+function PostCard({$id, title, image,}) {
+    
+    return (
+        <Link to={`/Post/${$id}`}>
+            <div className='w-full bg-gray-100 rounded-xl p-4'>
+                <div className='w-full justify-center mb-4'>
+                    <img src={appwriteService.getFilePreview(image)} alt={title} className='rounded-xl' />
+                </div>
+                <h2 className='text-xl font-bold'>{title}</h2>
+            </div>
+        </Link>
+    )
+}
+
+export default PostCard;
