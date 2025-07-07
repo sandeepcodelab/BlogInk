518155972fa85d381279920b89188f33e9185a1e
sandeepcodelab <sandeep.codes1@gmail.com>
2025-07-07 13:56:19 +0530

    add some new components

--- src/components/index.js
+++ src/components/index.js
@@ -3,5 +3,27 @@ import Footer from "./Footer/Footer";
 import LogoutBtn from "./Header/LogoutBtn";
 import Logo from "./Logo";
 import Container from "./Container/Container";
+import Button from "./Button";
+import Input from "./Input";
+import Select from "./Select";
+import RTE from "./RTE";
+import Signup from "./Signup";
+import Login from "./Login";
+import PostForm from "./post-form/PostForm";
+import PostCard from "./PostCard";
 
-export { Header, Footer, Container, Logo, LogoutBtn };
+export { 
+    Header,
+    Footer, 
+    Container, 
+    Logo, 
+    LogoutBtn,
+    Button,
+    Input,
+    Select,
+    RTE,
+    Signup,
+    Login,
+    PostForm,
+    PostCard
+};
