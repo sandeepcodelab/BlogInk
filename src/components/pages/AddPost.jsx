fc8d1ff634272a481596bdffa613171df220cba6
sandeepcodelab <sandeep.codes1@gmail.com>
2025-07-07 13:32:28 +0530

    add new pages

---
+++ src/components/pages/AddPost.jsx
@@ -0,0 +1,13 @@
+import { PostForm, Container } from "../index"
+
+function AddPost() {
+    return (
+        <div className="py-8">
+            <Container>
+                <PostForm />
+            </Container>
+        </div>
+    )    
+}
+
+export default AddPost
