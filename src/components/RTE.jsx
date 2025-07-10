2322c98c0185158a30ec211aef49987c76c3e04a
sandeepcodelab <sandeep.codes1@gmail.com>
2025-07-10 17:44:34 +0530

    add tiny API key

--- src/components/RTE.jsx
+++ src/components/RTE.jsx
@@ -1,5 +1,6 @@
 import { Editor } from '@tinymce/tinymce-react'
 import { Controller } from 'react-hook-form'
+import importEnv from '../config/importEnv'
 
 export default function RTE({name, control, label, defaultValue = ""}){
     return (
@@ -11,6 +12,7 @@ export default function RTE({name, control, label, defaultValue = ""}){
                 control={control}
                 render={({field: {onChange}}) => (
                     <Editor 
+                        apiKey={importEnv.tinyApiKey}
                         initialValue={defaultValue}
                         init={{
                             initialValue: defaultValue,
